3430b66f1ee6e1f1c3fc0af7006c523b950a601b
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-02-21 00:49:35 +0330

    config settings + gig models

--- gig/models.py
+++ gig/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Gig(models.Model):
+    slug = models.SlugField(unique=True)
+    cost = models.CharField(max_length=10)
+    description = models.TextField(max_length=1000)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    sell_count = models.CharField(max_length=10,default='0')
+    
