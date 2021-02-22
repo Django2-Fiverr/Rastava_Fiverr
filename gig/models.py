567821487c55679e963463ac821aae105fd3b096
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-02-23 03:12:49 +0330

    Config setting and urls and modify forms of user

--- gig/models.py
+++ gig/models.py
@@ -1,7 +1,34 @@
+import os
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 
+# use the custom user table ( the default one in settings.py) 
 User = get_user_model()
+
+
+# This function splits file name and its format ( one.jpg -> one + .jpg )
+def split_name(file_name):
+    base_name = os.path.basename(file_name)
+    name, format = os.path.splitext(base_name)
+    return name, format
+
+
+# This function changes default file name and uses the same format ( one.jpg -> two.jpg )
+# it returns an address to save the uploaded image file
+def get_name(instance, file_name):
+    name, format = split_name(file_name)
+    new_name = '{}-{}'.format(instance.id, format)
+    return 'gigs/{}'.format(new_name)
+
+
+# Gis manager class which customizes gig objects methods
+class GigManager(models.Manager):
+    def get_active_gigs(self):
+        return self.get_queryset().filter(active=True) # returns active ( available gigs )
+
+    def search(self,query): # This one requres category table ( going to be fixed later )
+        pass
 
 
 class Gig(models.Model):
@@ -9,5 +36,12 @@ class Gig(models.Model):
     cost = models.CharField(max_length=10)
     description = models.TextField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    sell_count = models.CharField(max_length=10,default='0')
-    
+    sell_count = models.IntegerField(default=0)
+    active = models.BooleanField(default=False)
+    image = models.ImageField(upload_to=get_name, null=True, blank=True)
+
+    objects = GigManager() # the previous class (Gig menager)
+
+    # returns the name of the gig owner
+    def __str__(self):
+        return f'{self.user.username}'
--- user/models.py
+++ user/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
-from django.utils import timezone
 
 
+
+
+# Create custom User table using required fields or overrdide predefiend fields
 class User(AbstractUser):
     first_name = models.CharField(_('first name'), max_length=150)
     last_name = models.CharField(_('last name'), max_length=150)
@@ -12,4 +14,8 @@ class User(AbstractUser):
         _('Phone number'), max_length=11, blank=True, null=True)
     age = models.CharField(_('age'), max_length=3, blank=True, null=True)
     date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
