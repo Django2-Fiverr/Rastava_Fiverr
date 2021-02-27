598d234bb626b746ec5e0d295b373f7a2cd445fa
Aryan-hab <67195127+Aryan-hab@users.noreply.github.com>
2021-02-27 12:00:53 +0330

    Fixed some issues

--- Fiverr/urls.py
+++ Fiverr/urls.py
@@ -13,16 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-<<<<<<< HEAD
 
+from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-<<<<<<< Updated upstream
 from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name='registration/login.html')), # for django-allauth
-=======
 from . import settings,views
 
 urlpatterns = [
@@ -30,22 +25,8 @@ urlpatterns = [
     path('', views.home_page),
     path('accounts/', include('user.urls')),
     path('gigs/', include('gig.urls')),
->>>>>>> Stashed changes
     path('admin/', admin.site.urls),
-     path('account/',include('django.contrib.auth.urls')),
-=======
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from . import settings,views
-
-urlpatterns = [
-    path('', views.home_page),
-    path('accounts/', include('user.urls')),
-    path('gigs/', include('gig.urls')),
-    path('admin/', admin.site.urls),
->>>>>>> sajad
-]
+    path('account/',include('django.contrib.auth.urls')),
 
 
 if settings.DEBUG:
