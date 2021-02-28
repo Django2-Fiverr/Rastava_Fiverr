bf21066458c1589b8f06e3935ab4fe9c0c50e5c3
azadeh <azadeh.ghahremani93@yahoo.com>
2021-02-28 14:51:12 +0330

    added comment

--- category/admin.py
+++ category/admin.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
-from .models import Category, Post
+from .models import Category
 from mptt.admin import MPTTModelAdmin
 
-class PostAdmin(admin.ModelAdmin):
-     list_display = ['title', 'category', 'content', 'slug']
-     
-admin.site.register(Post,PostAdmin)
-
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
+    list_display = ['name',]
     
 admin.site.register(Category , MPTTModelAdmin)
--- category/views.py
+++ category/views.py
@@ -1,19 +1,6 @@
 from django.shortcuts import render
-from category.models import Category,Post
+from category.models import Category
 from django.shortcuts import get_list_or_404
 
 def category(request,hierarchy=''):
-    category_slug = hierarchy.split('/')
-    parent = ''
     root = Category.objects.all()
-
-    for slug in category_slug[:-1]:
-        parent = root.get(parent=parent, slug = slug)
-
-    try:
-        instance = Category.objects.get(parent=parent,slug=category_slug[-1])
-    except:
-        instance = get_list_or_404(Post, slug = category_slug[-1])
-        return render(request, "postDetail.html", {'instance':instance})
-    else:
-        return render(request, 'categories.html', {'instance':instance})
